1a0acc55e1bd1ba148ae3a94f75e939762383fad
wangluming <ming845378603@126.com>
2021-08-05 16:11:58 +0800

    feat:补充相关参数

--- args_save.py
+++ args_save.py
@@ -4,7 +4,12 @@
 # # 输入雷达数据
 # parser.add_argument('--radar_data_path', default="input/data_XY_1225_Case17_frame0-299.mat")
 # radar_all_in_world = [my_file[element[0]][:] for element in my_file['data_XY']]
-
+# out_t = [-1, 2, 0]  # tx，ty,tz
+# out_angle = [-13, 0, 0]  # angle_x，angle_y,angle_z
+# in_matrix = [640, 360, 1500, 400]  # cx,cy,fx,fy
+# car_height = 1.5
+# camera_height = 2  # 相机地面高度
+# camera_angle_a = -1.5  # 相机光抽和水平线夹角
 
 # 1225_17_in_radar
 # # 输入视频数据
